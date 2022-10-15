5cec5b301b3de769afcc6a759a982c191a020763
It_Lilya <ihneva@bk.ru>
2022-10-15 22:48:30 +0300

    test app

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,13 +1,12 @@
 import { Character } from '../app';
 
-test('name in object', () => {
-  const received = new Character('B', 'Bowerman');
-  const expected = 'Ошибка в name'
+test('properties in app', () => {
+  const received = new Character('Boo', 'Bowerman');
+  const expected = {
+    name: 'Boo',
+    type: 'Bowerman',
+    health: 100,
+    level: 1,
+  }
   expect(received).toEqual(expected);
 });
-
-test('type in object', () => {
-  const received = new Character('Boo', 'bowerman');
-  const expected = 'Ошибка в types'
-  expect(received).toEqual(expected);
-})
